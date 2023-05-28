85ceb6266a2718befd80785fe8f1e906d97cf6fc
Nikolas Tsiongas <ntsiongas@gmail.com>
2023-05-28 18:54:58 +0200

    Fix menu after tailwind custom screens configuration

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -21,9 +21,9 @@ export default defineComponent({
         return () =>
             <nav class="bg-rose-900">
                 <div class="max-w-7xl mx-auto p-4">
-                    <div class="flex flex-col md:flex-row flex-gaps space-y-1 space-x-0 md:space-x-2">
+                    <div class="flex flex-col tablet:flex-row flex-gaps space-y-1 space-x-0 tablet:space-x-2">
                         {/* Mobile menu button */}
-                        <div class="-mr-2 flex md:hidden" onClick={smMenuActiveToggle}>
+                        <div class="-mr-2 flex tablet:hidden" onClick={smMenuActiveToggle}>
                             <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out" aria-label="Menu" aria-expanded="false">
                                 <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -41,7 +41,7 @@ export default defineComponent({
 
                             // Resolve mobile menu
                             classes += smMenuActive.value ? ' block' : ' hidden'
-                            classes += ' md:block'
+                            classes += ' tablet:block'
 
                             return <NuxtLink to={link.to} class={classes}>{link.title}</NuxtLink>
                         })}
