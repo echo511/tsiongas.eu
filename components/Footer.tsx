222ee0c6a31a56e4a4363e433004e06aac10ae45
Nikolas Tsiongas <ntsiongas@gmail.com>
2023-05-28 19:23:36 +0200

    Tweak footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,12 +5,9 @@ export default defineComponent({
         return <>
             <footer class="bg-stone-800 py-8 mt-auto">
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div class="flex justify-between items-center">
+                    <div class="grid justify-between items-center">
                         <div class="text-gray-400 font-medium text-sm">&copy; 2023 <span class="text-white">Nikolas Tsiongas</span>. All rights reserved.</div>
-                        <ul class="flex space-x-4">
-                            <li>
-                                <a target="_blank" href="#email" onclick="document.location='mailto:'+'ntsiongas'+'@'+'gmail.com'" class="text-gray-400 hover:text-gray-100 transition duration-200">Email</a>
-                            </li>
+                        <ul class="flex  space-x-4">
                             <li>
                                 <a target="_blank" href="https://github.com/echo511/" class="text-gray-400 hover:text-gray-100 transition duration-200">Github</a>
                             </li>
